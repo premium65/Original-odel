e9b8b76473dc2a69fa9b49499b75cb75de647a25
premium65 <premiumwork75@gmail.com>
2026-01-28 00:42:10 +0400

    Update app.ts

--- project/server/app.ts
+++ project/server/app.ts
@@ -29,12 +29,15 @@ declare module 'http' {
     rawBody: unknown
   }
 }
+
+// INCREASED BODY SIZE LIMIT TO 10MB for image uploads
 app.use(express.json({
+  limit: '10mb',
   verify: (req, _res, buf) => {
     req.rawBody = buf;
   }
 }));
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false, limit: '10mb' }));
 
 app.use((req, res, next) => {
   const start = Date.now();
@@ -95,9 +98,6 @@ export default async function runApp(
   await setup(app, server);
 
   // ALWAYS serve the app on the port specified in the environment variable PORT
-  // Other ports are firewalled. Default to 5000 if not specified.
-  // this serves both the API and the client.
-  // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || '5000', 10);
   server.listen({
     port,
