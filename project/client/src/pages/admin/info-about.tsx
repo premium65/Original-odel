0785c6e2bf8345c59e55770cccdfeacf6fca1240
premium65 <premiumwork75@gmail.com>
2026-01-27 21:24:31 +0400

    Update info-about.tsx

--- project/client/src/pages/admin/info-about.tsx
+++ project/client/src/pages/admin/info-about.tsx
@@ -31,6 +31,7 @@ export default function AdminInfoAbout() {
     mutationFn: async (data: typeof content) => {
       const res = await fetch("/api/admin/settings/pages", {
         method: "POST",
+        credentials: "include",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ type: "about", ...data }),
       });
