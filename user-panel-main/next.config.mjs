3127594f76f1b50596e6c07681821cd1a5b246d7
premium65 <premiumwork75@gmail.com>
2026-02-10 18:14:47 +0400

    optimize next config for memory

--- user-panel-main/next.config.mjs
+++ user-panel-main/next.config.mjs
@@ -1,13 +1,18 @@
-// next.config.js
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   reactStrictMode: true,
-  swcMinify: true, // Use SWC to minify your code
+  swcMinify: true,
   eslint: {
     ignoreDuringBuilds: true,
-},
-  // Optionally, add custom configurations here
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  experimental: {
+    workerThreads: false,
+    cpus: 1,
+  },
   images: {
     remotePatterns: [
       {
@@ -15,32 +20,25 @@ const nextConfig = {
         hostname: process.env.NEXT_PUBLIC_LOCAL_IMAGE_HOST || "localhost",
       },
       {
-		protocol: "https",
-		hostname: "api.goib.tech",		
-	},
-	{
-		protocol: "https",
-		hostname: "api.goibtech.online",
-
-	},	
-	{
-          protocol: 'https',
-          hostname: 'api.goibtech.site',
-        },
-	{
+        protocol: "https",
+        hostname: "api.goib.tech",
+      },
+      {
+        protocol: "https",
+        hostname: "api.goibtech.online",
+      },
+      {
+        protocol: "https",
+        hostname: "api.goibtech.site",
+      },
+      {
         protocol: "https",
         hostname: process.env.NEXT_PUBLIC_API_IMAGE_HOST || "api.goibtech.site",
-      }
+      },
     ],
   },
-  // Enable experimental features if needed (e.g., app directory)
-  // experimental: {
-  //   appDir: true,
-  // },
-
-  // Example: Adding environment variables
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL, // Available on the client and server
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
 };
 
